b36df054c3e9fece8ac926c86340d4c163efd3e7
Praveen Koujalagi <152680449+Praveen-koujalagi@users.noreply.github.com>
2025-07-27 11:25:11 +0530

    Time: 0 ms (100%), Space: 42 MB (13.73%) - LeetHub

--- 2316-count-hills-and-valleys-in-an-array/2316-count-hills-and-valleys-in-an-array.java
+++ 2316-count-hills-and-valleys-in-an-array/2316-count-hills-and-valleys-in-an-array.java
@@ -1,26 +1,27 @@
 class Solution {
     public int countHillValley(int[] nums) {
-        List<Integer> filtered = new ArrayList<>();
-        filtered.add(nums[0]);
-
-        // Remove consecutive duplicates
-        for (int i = 1; i < nums.length; i++) {
-            if (nums[i] != nums[i - 1]) {
-                filtered.add(nums[i]);
+        int c = 0;
+        for(int i=1;i<nums.length-1;i++) {
+            if(nums[i] == nums[i-1]) continue;
+            int val = nums[i];
+            int left = i-1;
+            int right = i+1;
+            while(left >= 0 && val == nums[left]) {
+                left--;
+            }
+            while(right < nums.length && val == nums[right]) {
+                right++;
+            }
+            
+            if(left >= 0 && right < nums.length) {
+                if(val > nums[left] && val > nums[right]) {
+                    c++;
+                } 
+                else if(val < nums[left] && val < nums[right]) {
+                    c++;
+                }
             }
         }
-
-        int count = 0;
-        for (int i = 1; i < filtered.size() - 1; i++) {
-            int prev = filtered.get(i - 1);
-            int curr = filtered.get(i);
-            int next = filtered.get(i + 1);
-
-            if ((curr > prev && curr > next) || (curr < prev && curr < next)) {
-                count++;
-            }
-        }
-
-        return count;
+        return c;
     }
 }
