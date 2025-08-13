5c7c25f493ba3540e90a0ba97deef4d9c4db2327
Praveen Koujalagi <152680449+Praveen-koujalagi@users.noreply.github.com>
2025-08-13 22:35:44 +0530

    Time: 22 ms (60.78%), Space: 51.7 MB (17.65%) - LeetHub

---
+++ 1662-minimum-numbers-of-function-calls-to-make-target-array/1662-minimum-numbers-of-function-calls-to-make-target-array.java
@@ -0,0 +1,19 @@
+class Solution {
+    public int minOperations(int[] nums) {
+	int ops = 0;
+
+	for (int bit = 0; bit < 32; bit++) {
+		boolean nonzero = false;
+
+		for (int i = 0; i < nums.length; i++) {
+			ops += nums[i] % 2;
+			nums[i] /= 2;
+			nonzero |= nums[i] > 0;
+		}
+
+		if (nonzero) ops++; else break;
+	}
+
+	return ops;
+}
+}
